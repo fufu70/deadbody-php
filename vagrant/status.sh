f3fd80550f3813b6ffaa65ff57559379044b8757
fufu70 <cmicklis@stetson.edu>
2017-02-28 21:07:49 -0500

    Added build file information to the deadbody

--- vagrant/status.sh
+++ vagrant/status.sh
@@ -2,4 +2,6 @@
 
 IP_ADDRESS=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
 
-sudo sudo -E bash $SHELL_HOME/install/helpers/output-block.sh "You can access your server by entering this IP in your browser: ${IP_ADDRESS}"
+sudo sudo -E bash $SHELL_HOME/install/helpers/output-block.sh "You can access your server by entering this IP in your browser: ${IP_ADDRESS}
+
+To Access Jenkins: 						${IP_ADDRESS}:8080"
