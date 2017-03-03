7c705038f6032acecd517ed465c6fcd624409abe
fufu70 <cmicklis@stetson.edu>
2017-03-02 22:33:31 -0500

    Created a dummy test cases and modified the build file to ingore some folders

--- src/protected/tests/WebTestCase.php
+++ src/protected/tests/WebTestCase.php
@@ -1,17 +1,11 @@
 <?php
 
 /**
- * Change the following URL based on your server configuration
- * Make sure the URL ends with a slash so that we can use relative URLs in test cases
- */
-define('TEST_BASE_URL','http://localhost/testdrive/index-test.php/');
-
-/**
  * The base class for functional test cases.
  * In this class, we set the base URL for the test application.
  * We also provide some common methods to be used by concrete test classes.
  */
-class WebTestCase extends CWebTestCase
+class WebTestCase extends \PHPUnit_Framework_TestCase
 {
 	/**
 	 * Sets up before each test method runs.
@@ -21,5 +15,10 @@ class WebTestCase extends CWebTestCase
 	{
 		parent::setUp();
 		$this->setBrowserUrl(TEST_BASE_URL);
+	}
+
+	public function test_true()
+	{
+		$this->assertTrue(true);
 	}
 }
